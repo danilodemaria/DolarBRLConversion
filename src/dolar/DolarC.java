32c450ed037f634ecfe78dc8c274b25892cee8df
Danilo de Maria <daniloctg@msn.com>
2018-02-13 21:32:02 -0200

    Commit inicial do projeto

---
+++ src/dolar/DolarC.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dolar;
+
+/**
+ *
+ * @author Administração
+ */
+public class DolarC {
+    private String valor;
+
+    /**
+     * @return the valor
+     */
+    public String getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+}
